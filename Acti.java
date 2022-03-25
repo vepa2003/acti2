308a0d0e400f92693fcb0d2729bd2a59947daa03
vepa2003 <101897191+vepa2003@users.noreply.github.com>
2022-03-25 19:12:49 +0100

    Update Acti.java

--- Acti.java
+++ Acti.java
@@ -1,3 +1,5 @@
 public class Acti{
-  System.out.println("Hola Jenkins, sóc el Didac Velasco Panisello");
+  public static void main(String[] args) {
+    System.out.println("Hola Jenkins, sóc el Didac Velasco Panisello");
   }
+}
